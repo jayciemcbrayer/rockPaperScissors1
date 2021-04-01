d821af85c956af717f880627ec92f05b0d65b4a0
Jaycie McBrayer <jaycielaynee@gmail.com>
2021-04-01 15:42:38 -0500

    adding bones for score keeping challenges

--- script.js
+++ script.js
@@ -30,54 +30,69 @@ console.log(document.getElementById("matchDiv"));
 
 //Task 2: Play Paper 
 document.querySelector(".paper").addEventListener('click', function(){
-    document.getElementById("playerHand").src = optionImages;
+    document.getElementById("playerHand").src = paperImage;
+    document.getElementById("compHand").src = optionImages[Math.floor(Math.random() * (3))];
 })
 //Task 3: Play Scissors
 document.querySelector(".scissors").addEventListener('click', function(){
     document.getElementById("playerHand").src = scissorsImage;
+    document.getElementById("compHand").src = optionImages[Math.floor(Math.random() * (3))];
 })
 
 //Task 4: Play Rock
 document.querySelector(".rock").addEventListener('click', function(){
     document.getElementById("playerHand").src = rockImage;
-})
-
-//CPU Plays
-Math.floor(Math.random() * (3));
-document.getElementById("compHand").addEventListener('click', function(){
     document.getElementById("compHand").src = optionImages[Math.floor(Math.random() * (3))];
 })
 
+//Rock Paper Scissors Rules
+// Rock > Scissors (+1 point)
+// Scissors > Paper (+1 point)
+// Paper > Rock (+1 point)
+// Rock = Rock (no points)
+// Paper = Paper (no points)
+// Scissors = Scissors (no points)
+//Use a comparison operator to determine match winner and add points to winner
+//Loop through game until one player reaches 5 points, then game reset
+//Score function nested inside a loop that breaks out once player_score = 5 || computer_score = 5;
+switch
+    case rock > scissors
+        rock + 1 point
+        break;
+    case Scissors > Paper
+        Scissors + 1 point
+        break;
+    case paper > rock
+        paper + 1
+        break;
+    case rock = rock
+        points + 0
+        break;
+    case paper = paper
+        points + 0
+        break;
+    case scissors = scissors
+        points + 0
+        break;
+
+//Score function nested inside a loop that breaks out once player_score = 5 || computer_score = 5;
+// if (player_score = 5 || computer_score = 5) {
+    //game reset
+// }
+// if (player_score = 5) {
+//     alert("Congratulations, you win!")
+// } else if (computer_score = 5)
+//     alert("Sorry, you lose!")
 
 
-// function playMatch () 
-//  if (player clicks rock)
-//  play rock
-//  else if (player clicks paper)
-//  play paper
-//  else 
-//  play scissors
 
-//  //CPU
-//  Math.floor(Math.random() * (max - min) + 1);
-//  //make a number between 1-3 correspond with an action button
-//  let a number (1) = an action (rock) ?
+//CPU Plays
+// Math.floor(Math.random() * (3));
+// document.getElementById("compHand").addEventListener('click', function(){
+//     document.getElementById("compHand").src = optionImages[Math.floor(Math.random() * (3))];
+// })
 
-//  //options
-// switch 
-//     case
-//     if ()
-//     else if ()
-//     else ()
-//     break;
-//     case
-//     if ()
-//     else if ()
-//     else ()
-//     break;
 
-// //nest varibles in an array
-// //let game = [rock, paper, scissors]
 
 // function checkRequired(arrayOfInputs) {
 //     arrayOfInputs.forEach((input) => {
